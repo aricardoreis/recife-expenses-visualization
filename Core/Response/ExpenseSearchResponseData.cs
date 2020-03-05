3f214604884fc1c7b9ebd753aa039e5a3264c934
Andre Reis <arrr.cin@gmail.com>
2020-03-05 12:34:24 -0300

    Create ExpenseSearchResponseData.cs
    This class represents the result data that comes from the API

---
+++ Core/Response/ExpenseSearchResponseData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Response
+{
+    public class ExpenseSearchResponseData
+    {
+        [JsonProperty("result")]
+        public ExpenseSearchResult Result { get; set; }
+    }
+
+    public class ExpenseSearchResult
+    {
+        [JsonProperty("records")]
+        public IList<ExpenseResultRecord> Records { get; set; }
+    }
+
+    public class ExpenseResultRecord
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("value")]
+        public decimal Value { get; set; }
+    }
+}
