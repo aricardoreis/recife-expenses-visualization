0234922c07670abc327009c7c892764639451973
Andre Reis <arrr.cin@gmail.com>
2020-03-05 15:54:19 -0300

    Add tests for the other methods of ExpenseService
    Mark a todo list to add additional checks.

--- test/IntegrationTests/ExpenseServiceTests.cs
+++ test/IntegrationTests/ExpenseServiceTests.cs
@@ -15,14 +15,36 @@ namespace IntegrationTests
         [Test]
         public async Task GetGroupedByMonth()
         {
-            ExpenseService service = new ExpenseService();
+            using var service = new ExpenseService();
             var result = await service.GetGroupedByMonth();
 
-            // 12 months a year
             Assert.IsNotNull(result);
+
+            // 12 months a year
             Assert.That(result.Count() == 12);
+            Assert.IsNotNull(result.FirstOrDefault());
             Assert.That(!string.IsNullOrEmpty(result.FirstOrDefault().Name));
             Assert.That(result.FirstOrDefault().Value > 0M);
+        }
+
+        [Test]
+        public async Task GetGroupedByCategory()
+        {
+            using var service = new ExpenseService();
+            var result = await service.GetGroupedByCategory();
+
+            // TODO: Add additional checks
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task GetGroupedBySource()
+        {
+            using var service = new ExpenseService();
+            var result = await service.GetGroupedBySource();
+
+            // TODO: Add additional checks
+            Assert.IsNotNull(result);
         }
     }
 }
