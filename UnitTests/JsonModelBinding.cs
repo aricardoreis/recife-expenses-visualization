0abc8c862b42f97efd37d212641a8c2bc2199f44
Andre Reis <arrr.cin@gmail.com>
2020-03-05 12:48:01 -0300

    Add a new project to create unit tests

---
+++ UnitTests/JsonModelBinding.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class JsonModelBinding
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
