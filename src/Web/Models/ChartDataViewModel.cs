266a5d1c6c2fefc348fc53dc7e00c474866fbd33
Andre Reis <arrr.cin@gmail.com>
2020-03-06 21:22:45 -0300

    Add chart views using Google Charts

---
+++ src/Web/Models/ChartDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ChartDataViewModel
+    {
+        public IEnumerable<BaseExpenseViewModel> Expenses { get; set; }
+        public ChartType Type { get; set; }
+        public string ColumnName { get; set; }
+    }
+}
--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿$(function () {
+﻿const chartTypes = {
+    LINE: 0,
+    PIE: 1,
+    COLUMN: 2,
+}
+
+google.charts.load('current', { 'packages': ['line', 'corechart'] });
+
+$(function () {
     $('.load-button').on('click', (e) => {
         e.preventDefault();
 
@@ -11,16 +19,60 @@
 
 function loadTableData(url, container) {
     addLoading(container);
-    $.get(url, function (data) {
+    $.get(url, (data) => {
         container.html(data);
         container.find('table').DataTable({
             "order": []
         });
+
+        $('#graph-button').on('click', () => configureAndDrawChart('chart'));
     });
 }
 
 function addLoading(container) {
     let spinner = $('<div>').addClass('text-center');
     $('<div>').addClass('spinner-border').appendTo(spinner);
+
     container.html(spinner);
+}
+
+function configureAndDrawChart(elementId) {
+    var columnName = $('#columnName').val();
+
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn('string', 'Data');
+    dataTable.addColumn('number', 'Expenses');
+
+    var data = loadExpenseData();
+    var arrayData = [];
+    for (let i = 0; i < data.length; i++) {
+        arrayData.push([data[i][columnName], data[i].Value]);
+    }
+
+    dataTable.addRows(arrayData);
+
+    var options = {
+        height: 400
+    };
+
+    var chartType = parseInt($('#chartType').val());
+    var chart = null;
+
+    switch (chartType) {
+        case chartTypes.LINE:
+            chart = new google.charts.Line(document.getElementById(elementId));
+            break;
+        case chartTypes.PIE:
+            chart = new google.visualization.PieChart(document.getElementById(elementId));
+            break;
+        case chartTypes.COLUMN:
+            chart = new google.visualization.ColumnChart(document.getElementById(elementId));
+            break;
+    }
+
+    chart.draw(dataTable, options);
+}
+
+function loadExpenseData() {
+    return JSON.parse($('#expenseData').val());
 }
